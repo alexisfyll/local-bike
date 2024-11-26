87b7241ee3b246a49a70b77917a422499cf14efa
Alexis Fayolle <alexisfayolle@yahoo.fr>
2024-11-26 16:27:03 +0000

    list price buckets

--- models/mart/mrt_local_bike__monthly_sales.sql
+++ models/mart/mrt_local_bike__monthly_sales.sql
@@ -4,6 +4,12 @@ SELECT
     , category_name
     , brand_name
     , store_name
+    , CASE WHEN unit_list_price < 500 THEN '0-500'
+            WHEN unit_list_price < 1000 THEN '500-1000'
+            WHEN unit_list_price < 1500 THEN '1000-1500'
+            WHEN unit_list_price < 2000 THEN '1500-2000'
+            ELSE '>2000'
+            END as list_price_bucket
     , SUM(quantity) as quantity
     , SUM(discounted_price) as total_sales_amount
     , SUM(unit_list_price*quantity) as total_listed_price
